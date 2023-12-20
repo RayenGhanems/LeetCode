0398deb5c5471f4c6bcf6572f57fc7c135f94e6b
Rayen ghanem <144811715+RayenGhanems@users.noreply.github.com>
2023-12-20 21:55:26 +0100

    Time: 44 ms (32.54%), Space: 13.1 MB (87.57%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,6 +1,15 @@
 class Solution(object):
     def buyChoco(self, prices, money):
-        prices = sorted(prices)
-        if prices[0]+prices[1]>money:
-            return money
-        return money -(prices[0]+prices[1])
+        one = prices[0] 
+        two = prices[1]
+
+        for i in prices[2:]:
+            if i < max(one,two):
+                if one>two:
+                    one=i
+                else:
+                    two =i
+
+        result = money if (one + two) > money else money - (one + two)
+        return result
+
