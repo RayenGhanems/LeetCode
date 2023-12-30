936070a5f66d3eb33a3484ef28a7b54f335fce7a
Rayen ghanem <144811715+RayenGhanems@users.noreply.github.com>
2023-12-30 13:10:30 +0100

    Time: 61 ms (30.00%), Space: 13.5 MB (100.00%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.py
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def makeEqual(self, words):
-        dic = {chr(letter): 0 for letter in range(ord('a'), ord('z') + 1)}
+        dic = defaultdict(int)
         for word in words:
             for char in word:
                 dic[char] += 1
         l = len(words)
-        for letter in range(ord('a'), ord('z') + 1):
-            if dic[chr(letter)] % l != 0:
+        for val in dic.values():
+            if val % l != 0:
                 return False
         return True
