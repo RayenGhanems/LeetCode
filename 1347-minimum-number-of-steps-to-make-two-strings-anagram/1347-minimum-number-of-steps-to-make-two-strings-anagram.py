f0696d13e3f6096b48417bf4961fcfaa7a29026c
Rayen ghanem <144811715+RayenGhanems@users.noreply.github.com>
2024-01-13 21:31:52 +0100

    Time: 160 ms (69.79%), Space: 14.2 MB (82.01%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def minSteps(self, s, t):
         dicts={}
-        out=0
         for char in s:
             if char in dicts:
                 dicts[char]+=1
@@ -10,6 +9,5 @@ class Solution(object):
         for char in t:
             if char in dicts and dicts[char]>0:
                 dicts[char]-=1
-        for i in dicts.values():
-            out+=i
+        out =sum(dicts.values())
         return out
