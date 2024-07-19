ad9a4e5377875fbc4504ed77f95ee6d77713dee7
Rayen ghanem <144811715+RayenGhanems@users.noreply.github.com>
2024-07-19 13:50:51 +0200

    Time: 15 ms (64.89%), Space: 14.3 MB (18.75%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
@@ -1,26 +1,37 @@
+#include <set>
+
 class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
         vector <int> out;
+        set<int> rows, cols;
         for(int i=0;i<m;i++){
-            for(int j=0; j<n;j++){
-                bool acceptable = 1;
-                for(int k = 0;k<m;k++){
-                    if(matrix[k][j]>matrix[i][j]){
-                        acceptable = 0;
-                        break;
-                    }
-                }
-                if(acceptable){
-                    for(int k = 0;k<n;k++){
-                        if(matrix[i][k]<matrix[i][j]){
-                            acceptable = 0;
-                            break;
+            if(!(rows.find(i) != rows.end())){
+                for(int j=0; j<n;j++){
+                    if(!(cols.find(j) != cols.end())){
+                        bool acceptable = 1;
+                        for(int k = 0;k<m;k++){
+                            if(matrix[k][j]>matrix[i][j]){
+                                acceptable = 0;
+                                break;
+                            }
+                        }
+                        if(acceptable){
+                            for(int k = 0;k<n;k++){
+                                if(matrix[i][k]<matrix[i][j]){
+                                    acceptable = 0;
+                                    break;
+                                }
+                            }
+                        }
+                        if(acceptable){
+                            out.push_back(matrix[i][j]);
+                            rows.insert(i);
+                            cols.insert(j);
                         }
                     }
                 }
-                if(acceptable) out.push_back(matrix[i][j]);
             }
         }
         return out;
