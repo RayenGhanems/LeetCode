06f4b8c994e879c36cab0fc2f98d30ab80e3ebad
Rayen ghanem <144811715+RayenGhanems@users.noreply.github.com>
2025-12-10 16:37:42 +0100

    Time: 3135 ms (5.55%), Space: 41.8 MB (8.89%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
@@ -1,10 +1,13 @@
 class Solution:
     def countPermutations(self, com: List[int]) -> int:
+
         def factorial(x:int)-> int:
             if x == 1:
                 return 1
             return x * factorial(x-1)
+
         if com[0] != min(com) or com.count(com[0]) > 1:
             return 0
+            
         return factorial(len(com) -1) % ((10**9)+7)
         
