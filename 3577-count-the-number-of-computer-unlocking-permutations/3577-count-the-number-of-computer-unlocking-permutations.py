9faa842a0720c7ac7200cda53b48a84d1c5bf2d9
Rayen ghanem <144811715+RayenGhanems@users.noreply.github.com>
2025-12-10 16:39:48 +0100

    Time: 3093 ms (6.67%), Space: 42.5 MB (8.33%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
@@ -5,9 +5,10 @@ class Solution:
             if x == 1:
                 return 1
             return x * factorial(x-1)
+        x=com[0]
+        for i in com[1:]:
+            if i<= x:
+                return 0
 
-        if com[0] != min(com) or com.count(com[0]) > 1:
-            return 0
-            
         return factorial(len(com) -1) % ((10**9)+7)
         
