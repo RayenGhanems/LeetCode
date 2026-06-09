81d0e4f3cd24b141ebc47dfbc1407f28abff3ddd
Rayen ghanem <144811715+RayenGhanems@users.noreply.github.com>
2026-06-09 14:49:03 +0200

    Time: 16 ms (42.4%), Space: 26.5 MB (33.6%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.py
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxTotalValue(self, nums: List[int], k: int) -> int:
-        m,n = 0, 10**9
+        m,n = nums[0],nums[0]
         for i in nums:
             if i>m:
                 m=i
-            if i <n:
+            elif i <n:
                 n=i
         return k*(m-n)
