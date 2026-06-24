71e2385cefc862784f62ff15e433316547ae9b19
Rayen ghanem <144811715+RayenGhanems@users.noreply.github.com>
2026-06-24 13:01:31 +0200

    Time: 56 ms (70.2%), Space: 62.5 MB (43.66%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -5,22 +5,38 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        l = []
-        while head != None:
-            l.append(head.val)
-            head = head.next
+        ############################# solution 1
+        # l = []
+        # while head != None:
+        #     l.append(head.val)
+        #     head = head.next
 
-        n=len(l)  ## which is an even number
-        
-        if n ==2:
-            return sum(l)
+        # n=len(l)  ## which is an even number
 
-        l = [a + b for a, b in zip(l[:n//2], l[-1:n//2 - 1:-1])]
-        return max(l)
+        # if n ==2:
+        #     return sum(l)
 
+        # l = [a + b for a, b in zip(l[:n//2], l[-1:n//2 - 1:-1])]
+        # return max(l)
+        ################################# solution 2
         # out = l[0]+ l[-1]
         # for i in range(1,int(len(l)/2)):
         #     out = max(out, l[i]+l[-(i+1)])
         # return out
         
+        ################################## solution 3
+        l, slow, fast, i= [], head, head, 1
+        while fast and fast.next:
+            fast = fast.next.next
+            l.append(slow.val)
+            slow = slow.next
         
+        out = slow.val + l[-i]
+        while slow.next:
+            i += 1
+            slow = slow.next
+            out = max(out, slow.val + l[-i])
+        return out
+
+        
+            
