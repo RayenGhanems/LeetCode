8a3d6260e3674bb9ceda435248fb9f2b1fb6166c
Rayen ghanem <144811715+RayenGhanems@users.noreply.github.com>
2026-06-24 13:02:27 +0200

    Time: 59 ms (64.03%), Space: 62.8 MB (36.44%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -5,19 +5,18 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
+
         ############################# solution 1
-        # l = []
-        # while head != None:
-        #     l.append(head.val)
-        #     head = head.next
+        l = []
+        while head != None:
+            l.append(head.val)
+            head = head.next
+        n=len(l)  ## which is an even number
+        if n ==2:
+            return sum(l)
+        l = [a + b for a, b in zip(l[:n//2], l[-1:n//2 - 1:-1])]
+        return max(l)
 
-        # n=len(l)  ## which is an even number
-
-        # if n ==2:
-        #     return sum(l)
-
-        # l = [a + b for a, b in zip(l[:n//2], l[-1:n//2 - 1:-1])]
-        # return max(l)
         ################################# solution 2
         # out = l[0]+ l[-1]
         # for i in range(1,int(len(l)/2)):
@@ -25,18 +24,18 @@ class Solution:
         # return out
         
         ################################## solution 3
-        l, slow, fast, i= [], head, head, 1
-        while fast and fast.next:
-            fast = fast.next.next
-            l.append(slow.val)
-            slow = slow.next
+        # l, slow, fast, i= [], head, head, 1
+        # while fast and fast.next:
+        #     fast = fast.next.next
+        #     l.append(slow.val)
+        #     slow = slow.next
         
-        out = slow.val + l[-i]
-        while slow.next:
-            i += 1
-            slow = slow.next
-            out = max(out, slow.val + l[-i])
-        return out
+        # out = slow.val + l[-i]
+        # while slow.next:
+        #     i += 1
+        #     slow = slow.next
+        #     out = max(out, slow.val + l[-i])
+        # return out
 
         
             
